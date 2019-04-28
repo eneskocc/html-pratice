30ed36afbba53d27f2da5eabcf2aaea4d02817d1
Enes koç <32553624+enskocc12@users.noreply.github.com>
2019-04-28 13:00:31 +0300

    Update index.js

--- pratice5/index.js
+++ pratice5/index.js
@@ -27,22 +27,11 @@ function deleteText(){
     document.getElementById("text").value=" ";
 }
 
-var islemler = new Array();
-var i=0;
 
 function hesapla(){
     var $agee = document.getElementById("text").value;
     var $age = document.getElementById("text").value=eval(document.getElementById("text").value);
     document.getElementById("text").value=" "+$age;
-    islemler[i]=$agee+'='+$age;
-    i++;
-}
-
-function islemler() {
-    var dongu=islemler.length;
-    for (var i = 0; i < dongu; i++) {
-        document.write("<p>");
-              document.write(islemler[i]);
-        document.write("</p>");
-    }
+    var $ag = document.getElementById("islem").value;
+    var x = document.getElementById("islem").value= $ag+"\n"+$agee+" = "+$age;
 }
