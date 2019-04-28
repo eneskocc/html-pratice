76713d9be157e77b4aaa3877e8191fc2eb7d243e
Enes koç <32553624+enskocc12@users.noreply.github.com>
2019-04-29 00:01:30 +0300

    Add files via upload

---
+++ pratice7/index.js
@@ -0,0 +1,74 @@
+function tamam(){
+  var $agee = document.getElementById("username").value;
+}
+function tıkla(){
+  var $agee = document.getElementById("username").value;
+  var $age = document.getElementById("username").value=eval(document.getElementById("username").value);
+
+
+  document.getElementById("username").value=$agee+" = "+$age;
+}
+function tıkla1(){
+  var $age = document.getElementById("username").value;
+  document.getElementById("username").value= $age+1;
+}
+function tıkla2(){
+  var $age = document.getElementById("username").value;
+  document.getElementById("username").value= $age+2;
+}
+function tıkla3(){
+  var $age = document.getElementById("username").value;
+  document.getElementById("username").value= $age+3;
+}
+function tıkla4(){
+  var $age = document.getElementById("username").value;
+  document.getElementById("username").value= $age+" "+4;
+}
+function tıkla5(){
+  var $age = document.getElementById("username").value;
+  document.getElementById("username").value= $age+5;
+}
+function tıkla6(){
+  var $age = document.getElementById("username").value;
+  document.getElementById("username").value= $age+6;
+}
+function tıkla7(){
+  var $age = document.getElementById("username").value;
+document.getElementById("username").value= $age+7;
+}
+function tıkla8(){
+  var $age = document.getElementById("username").value;
+  document.getElementById("username").value= $age+8;
+}
+function tıkla9(){
+  var $age = document.getElementById("username").value;
+  document.getElementById("username").value= $age+9;
+}
+function tıkla0(){
+  var $age = document.getElementById("username").value;
+  document.getElementById("username").value= $age+0;
+}
+function tıkladelete(){
+  var $age = document.getElementById("username").value;
+  document.getElementById("username").value=" ";
+}
+function tıklafirgul(){
+  var $age = document.getElementById("username").value;
+  document.getElementById("username").value= $age+",";
+}
+function tıklatoplama(){
+  var $age = document.getElementById("username").value;
+  document.getElementById("username").value= $age+" "+"+ ";
+}
+function tıklacıkarma(){
+  var $age = document.getElementById("username").value;
+  document.getElementById("username").value= $age+" " + "- ";
+}
+function tıklacarp(){
+  var $age = document.getElementById("username").value;
+  document.getElementById("username").value= $age+" "+"* ";
+}
+function tıklabol(){
+  var $age = document.getElementById("username").value;
+  document.getElementById("username").value= $age+ " "+"/ ";
+}
